bb33ea31a4f4953569c6d3d14dd8d700ea905765
dongchangYoo <44886770+dongchangYoo@users.noreply.github.com>
2022-12-04 23:31:14 +0900

    Update setup.py
    change the version in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="bifrost-python-lib",
-    version="v0.2.0",
+    version="0.3.0",
     package=find_packages,
     install_requires=[
         "cryptography",
