58b1f3f58fb2beee50248d4a2d8233263d552ad3
dongchangYoo <dongchang.yoo@pi-lab.io>
2022-12-04 23:19:14 +0900

    add requirements.txt

--- setup.py
+++ setup.py
@@ -4,5 +4,13 @@ setup(
     name="bifrost-python-lib",
     version="v0.2.0",
     package=find_packages,
-    install_requires=["cryptography", "ecdsa", "eth-abi", "eth-account", "pycryptodome", "requests", "dataclasses-json"]
+    install_requires=[
+        "cryptography",
+        "ecdsa",
+        "eth-abi",
+        "eth-account",
+        "pycryptodome",
+        "requests",
+        "dataclasses-json"
+    ]
 )
