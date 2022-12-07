fd85bc9c3c43ad7ca6f2f8447b8993cc88c74c27
dongchangYoo <yoodc8167@gmail.com>
2022-12-07 17:40:10 +0900

    change package name

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from chainpy.__init__ import __version__
 
 setup(
-    name="bifrost-python-lib",
+    name="chainpy",
     version=__version__,
     package=find_packages,
     install_requires=[
