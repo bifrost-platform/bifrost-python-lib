8cde5b5eb4486e22b86b9eab5cfffa947601f047
dongchangYoo <yoodc8167@gmail.com>
2022-12-23 13:43:48 +0900

    BN-716 Rpc checker is divided by chain

--- chainpy/prometheus_metric.py
+++ chainpy/prometheus_metric.py
@@ -1,4 +1,8 @@
+from typing import Dict
+
 from prometheus_client import Gauge, start_http_server
+
+from chainpy.eth.ethtype.consts import ChainIndex
 
 
 class PrometheusExporter:
@@ -7,8 +11,8 @@ class PrometheusExporter:
 
     MONITOR_THREAD_ALIVE = Gauge("monitor_alive", "Description")
     SENDER_THREAD_ALIVE = Gauge("sender_alive", "Description")
-    RPC_FAILED = Gauge("rpc_request_failures", "Description")
-    RPC_REQUESTED = Gauge("rpc_requested", "Description")
+    RPC_REQUESTED: Dict[ChainIndex, Gauge] = dict()
+    RPC_FAILED: Dict[ChainIndex, Gauge] = dict()
 
     @staticmethod
     def init_prometheus_exporter(port: int = 8000):
@@ -28,13 +32,30 @@ class PrometheusExporter:
             raise Exception("Not supported thread_type")
 
     @staticmethod
-    def exporting_rpc_requested():
+    def exporting_rpc_requested(chain_index: ChainIndex):
         if not PrometheusExporter.PROMETHEUS_ON:
             return
-        PrometheusExporter.RPC_REQUESTED.inc()
+
+        chain_name = chain_index.name.lower()
+        gauge = PrometheusExporter.RPC_REQUESTED.get(chain_index)
+        if gauge is None:
+            PrometheusExporter.RPC_REQUESTED[chain_index] = Gauge(
+                "rpc_failures_on_{}".format(chain_name),
+                "Description"
+            )
+        PrometheusExporter.RPC_REQUESTED[chain_index].inc()
 
     @staticmethod
-    def exporting_rpc_failed():
+    def exporting_rpc_failed(chain_index: ChainIndex):
         if not PrometheusExporter.PROMETHEUS_ON:
             return
-        PrometheusExporter.RPC_FAILED.inc()
+
+        gauge = PrometheusExporter.RPC_FAILED.get(chain_index)
+        chain_name = chain_index.name.lower()
+        if gauge is None:
+            PrometheusExporter.RPC_FAILED[chain_index] = Gauge(
+                "rpc_failures_on_{}".format(chain_name),
+                "Description"
+            )
+
+        PrometheusExporter.RPC_FAILED[chain_index].inc()
