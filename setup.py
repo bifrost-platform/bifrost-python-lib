30ca39691852b5e984f54181ae1a58f827fa6d95
dongchangYoo <yoodc8167@gmail.com>
2023-01-05 13:35:48 +0900

    BN-873 binding to solidity-contract-config@230105, and version up to 0.5.3

--- setup.py
+++ setup.py
@@ -13,8 +13,6 @@ setup(
         "requests",
         "dataclasses-json",
         "prometheus-client",
-        "bridgeconst @ git+https://github.com/bifrost-platform/solidity-contract-configs@230104"
-
-
+        "bridgeconst @ git+https://github.com/bifrost-platform/solidity-contract-configs@230105"
     ]
 )
