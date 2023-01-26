dbe290e0576365c379a1b8c0ab2a8f3460416ec5
dongchangYoo <yoodc8167@gmail.com>
2023-01-26 12:31:44 +0900

    BN-930 add version string to each dependancy

--- setup.py
+++ setup.py
@@ -6,13 +6,14 @@ setup(
     version=__version__,
     package=find_packages,
     install_requires=[
-        "eth-abi",
-        "eth-keys",
-        "eth-account",
-        "pycryptodome",
-        "requests",
-        "dataclasses-json",
-        "prometheus-client",
-        "bridgeconst @ git+https://github.com/bifrost-platform/solidity-contract-configs@230119"
+        "eth-abi==3.0.1",
+        "eth-keys==0.4.0",
+        "eth-account==0.8.0",
+        "pycryptodome==3.16.0",
+        "requests==2.28.2",
+        "dataclasses-json==0.5.7",
+        "prometheus-client==0.15.0",
+        "bridgeconst @ git+https://github.com/bifrost-platform/solidity-contract-configs@230119",
+        "python-dotenv==0.21.0"
     ]
 )
