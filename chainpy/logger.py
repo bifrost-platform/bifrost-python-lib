b5d8a4eacf5a2813e806391dd411d3a3e745e2c0
dongchangYoo <yoodc8167@gmail.com>
2023-01-27 17:00:50 +0900

    BN-961 add option: exporting log to file

--- chainpy/logger.py
+++ chainpy/logger.py
@@ -5,11 +5,36 @@ from chainpy.eth.ethtype.hexbytes import EthAddress
 
 
 class LoggerSetting:
-    LEVEL = logging.DEBUG
-    FILENAME = None
-    MAX_BYTES = 10 * 1024 * 1024
-    BACKUP_COUNT = 10
-    FORMAT = "%(asctime)s [%(name)-10s] %(message)s"
+    def __init__(
+            self,
+            level: int = logging.DEBUG,
+            file_path: str = None,
+            max_bytes: int = 10 * 1024 * 1024,
+            backup_count: int = 10,
+            log_format: str = "%(asctime)s [%(name)-10s] %(message)s"
+    ):
+        self.level = level
+        self.file_path = file_path
+        self.max_bytes = max_bytes
+        self.backup_count = backup_count
+        self.log_format = log_format
+
+    def reset(
+            self,
+            level: int = logging,
+            file_path: str = None,
+            max_bytes: int = 10 * 1024 * 1024,
+            backup_count: int = 10,
+            log_format: str = "%(asctime)s [%(name)-10s] %(message)s"
+    ):
+        self.level = level
+        self.file_path = file_path
+        self.max_bytes = max_bytes
+        self.backup_count = backup_count
+        self.log_format = log_format
+
+
+logger_setting_global = LoggerSetting()
 
 
 def formatted_log(
@@ -29,24 +54,22 @@ def formatted_log(
     logger_obj.info(msg)
 
 
-def Logger(
-        name: str, level=LoggerSetting.LEVEL,
-        max_bytes=LoggerSetting.MAX_BYTES, _format=LoggerSetting.FORMAT, file_path=LoggerSetting.FILENAME):
-    # generate logger with "name"
+def Logger(name: str, level=logger_setting_global.level):
+    """ generate logger with "name" """
     logger = logging.getLogger(name)
     logger.setLevel(level)
 
     # define formatter and handler
-    formatter = logging.Formatter(_format)
+    formatter = logging.Formatter(logger_setting_global.log_format)
     stream_handler = logging.StreamHandler()
     stream_handler.setFormatter(formatter)
     logger.addHandler(stream_handler)
 
-    if file_path is not None:
+    if logger_setting_global.file_path is not None:
         file_handler = logging.handlers.RotatingFileHandler(
-            filename=file_path,
-            maxBytes=max_bytes,
-            backupCount=LoggerSetting.BACKUP_COUNT
+            filename=logger_setting_global.file_path,
+            maxBytes=logger_setting_global.max_bytes,
+            backupCount=logger_setting_global.backup_count
         )
 
         file_handler.setFormatter(formatter)
