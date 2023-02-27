caa613ea649a41bc7b614c0e41773652459e8a6d
dongchangYoo <yoodc8167@gmail.com>
2023-02-27 16:15:34 +0900

    CRQ-3 At the start, we prepared a unittest to launch the mocking rpc server.

---
+++ tests/rpcendpointmock/procutil.py
@@ -0,0 +1,28 @@
+import psutil
+
+
+def kill_by_file_name(file_name: str) -> bool:
+    for proc in psutil.process_iter():
+        try:
+            process_name = proc.name()
+            process_id = proc.pid
+
+            if process_name.startswith("Python"):
+                command_line = proc.cmdline()
+
+                if file_name in command_line:
+                    parent_pid = process_id
+                    parent = psutil.Process(parent_pid)
+
+                    for child in parent.children(recursive=True):
+                        child.kill()
+                    parent.kill()
+                    return True
+
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):   #예외처리
+            pass
+    return False
+
+
+if __name__ == "__main__":
+    kill_by_file_name("tests/rpcendpointmock/rpcserver.py")
