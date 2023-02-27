b3843c6d2a765d1369cb2d441a7c5d66e26fa32c
dongchangYoo <yoodc8167@gmail.com>
2023-02-27 11:39:12 +0900

    hotfix: The package version was adjusted to be compatible with eth-brownie v1.19.3.

--- setup.py
+++ setup.py
@@ -6,14 +6,13 @@ setup(
     version=__version__,
     package=find_packages,
     install_requires=[
-        "eth-abi==2.2.1",
-        "eth-keys==0.4.0",
-        "eth-account==0.8.0",
-        "pycryptodome==3.16.0",
-        "requests==2.28.2",
+        "eth-abi==2.2.0",
+        "eth-keys==0.3.4",
+        "eth-account==0.5.9",
+        "requests==2.28.1",
         "dataclasses-json==0.5.7",
         "prometheus-client==0.15.0",
         "bridgeconst @ git+https://github.com/bifrost-platform/solidity-contract-configs@0.2.1",
-        "python-dotenv==0.21.0"
+        "python-dotenv==0.16.0"
     ]
 )
