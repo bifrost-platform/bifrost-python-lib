6c1d70907208565cc8880a2bc9d266d6e34f4304
dongchangYoo <yoodc8167@gmail.com>
2023-03-02 12:58:01 +0900

    CRQ-8 refactoring rpchandler and add its test with rpcserver mock

--- chainpy/eth/managers/rpchandler.py
+++ chainpy/eth/managers/rpchandler.py
@@ -1,14 +1,13 @@
 import json
 import requests
 import time
-from typing import List, Optional, Union, Callable
+from typing import List, Optional, Union
 from json import JSONDecodeError
 from requests import Response
 
-from bridgeconst.consts import Chain
-
+from .consts import *
 from .exceptions import raise_integrated_exception, RpcOutOfStatusCode, RpCMaxRetry
-from .utils import merge_dict, reduce_height_to_matured_height, hex_height_or_latest
+from .utils import merge_dict, hex_height_or_latest
 from ..ethtype.amount import EthAmount
 from ..ethtype.hexbytes import EthAddress, EthHashBytes, EthHexBytes
 from ..ethtype.block import EthBlock
@@ -17,14 +16,6 @@ from ..ethtype.receipt import EthReceipt, EthLog
 from ..ethtype.transaction import EthTransaction
 from ...logger import global_logger
 from ...prometheus_metric import PrometheusExporter
-
-RPC_RETRY_MAX_RETRY_NUM = 20
-RPC_RETRY_SLEEP_TIME_IN_SECS = 180
-DEFAULT_RECEIPT_MAX_RETRY: int = 10
-DEFAULT_BLOCK_PERIOD_SECS: int = 3
-DEFAULT_BLOCK_AGING_BLOCKS: int = 1
-DEFAULT_RPC_DOWN_ALLOW_SECS: int = 180
-DEFAULT_RPC_TX_BLOCK_DELAY: int = 2
 
 
 class EthRpcClient:
@@ -41,19 +32,19 @@ class EthRpcClient:
     def __init__(
             self,
             url_with_access_key: str,
-            chain: Chain = Chain.NONE,
+            chain: str = DEFAULT_CHAIN_NAME,
             receipt_max_try: int = DEFAULT_RECEIPT_MAX_RETRY,
             block_period_sec: int = DEFAULT_BLOCK_PERIOD_SECS,
             block_aging_period: int = DEFAULT_BLOCK_AGING_BLOCKS,
-            rpc_server_downtime_allow_sec: int = DEFAULT_RPC_DOWN_ALLOW_SECS,
+            rpc_server_downtime_allow_sec: int = DEFAULT_RPC_RESEND_DELAY_SEC,
             transaction_block_delay: int = DEFAULT_RPC_TX_BLOCK_DELAY
     ):
-        self.__chain = chain
+        self.__chain: str = chain
         self.__url_with_access_key = url_with_access_key
         self.__receipt_max_try = DEFAULT_RECEIPT_MAX_RETRY if receipt_max_try is None else receipt_max_try
         self.__block_period_sec = DEFAULT_BLOCK_PERIOD_SECS if block_period_sec is None else block_period_sec
         self.__block_aging_period = DEFAULT_BLOCK_AGING_BLOCKS if block_aging_period is None else block_aging_period
-        self.__rpc_server_downtime_allow_sec = DEFAULT_RPC_DOWN_ALLOW_SECS \
+        self.__rpc_server_downtime_allow_sec = DEFAULT_RPC_RESEND_DELAY_SEC \
             if rpc_server_downtime_allow_sec is None else rpc_server_downtime_allow_sec
         self.__transaction_block_delay = DEFAULT_RPC_TX_BLOCK_DELAY \
             if transaction_block_delay is None else transaction_block_delay
@@ -68,23 +59,19 @@ class EthRpcClient:
             self.__chain_id = int(resp, 16)
 
     @classmethod
-    def from_config_dict(cls, config: dict, private_config: dict = None, chain_index: Chain = None):
+    def from_config_dict(cls, config: dict, private_config: dict = None, chain: str = DEFAULT_CHAIN_NAME):
         merged_config = merge_dict(config, private_config)
 
-        if merged_config.get("chain_name") is None and chain_index is None:
+        if merged_config.get("chain_name") is None and chain is None:
             # multichain config and no chain index
             raise Exception("should be inserted chain config")
 
-        if chain_index is None:
-            # in case of being inserted a chain config without chain index
-            chain_index = Chain[merged_config["chain_name"].upper()]
-
         if merged_config.get("chain_name") is None:
-            merged_config = merged_config[chain_index.name.upper()]
+            merged_config = merged_config[chain.upper()]
 
         return cls(
             merged_config["url_with_access_key"],
-            chain_index,
+            chain,
             merged_config.get("receipt_max_try"),
             merged_config.get("block_period_sec"),
             merged_config.get("block_aging_period"),
@@ -92,7 +79,7 @@ class EthRpcClient:
         )
 
     @classmethod
-    def from_config_files(cls, config_file: str, private_config_file: str = None, chain: Chain = Chain.NONE):
+    def from_config_files(cls, config_file: str, private_config_file: str = None, chain: str = DEFAULT_CHAIN_NAME):
         with open(config_file, "r") as f:
             config = json.load(f)
         if private_config_file is None:
@@ -111,11 +98,19 @@ class EthRpcClient:
         self.__url_with_access_key = url
 
     @property
+    def block_aging_period(self) -> int:
+        return self.__block_aging_period
+
+    @property
+    def resend_delay_sec(self) -> int:
+        return self.__rpc_server_downtime_allow_sec
+
+    @property
     def tx_commit_time_sec(self) -> int:
         return self.__block_period_sec * (self.__transaction_block_delay + self.__block_aging_period)
 
     @property
-    def chain(self) -> Chain:
+    def chain(self) -> str:
         """ return chain index specified from the configuration. """
         return self.__chain
 
@@ -124,19 +119,8 @@ class EthRpcClient:
         """ return chain id emitted by the rpc node. """
         return self.__chain_id
 
-    def send_request_base(self, body: dict, headers: dict) -> Response:
-        PrometheusExporter.exporting_rpc_requested(self.chain)
-        self.call_num += 1
-
-        response = requests.post(self.url, json=body, headers=headers)
-        code = response.status_code
-        if code < 200 or 400 < code:
-            raise RpcOutOfStatusCode(self.chain, "code({}), msg({})".format(code, response.content))
-
-        return response
-
-    def send_request(self, method: str, params: list, cnt: int = 0) -> Optional[Union[dict, str]]:
-        if cnt > RPC_RETRY_MAX_RETRY_NUM:
+    def send_request_base(self, method: str, params: list, cnt: int = 0) -> Response:
+        if cnt > RPC_MAX_RESEND_ITER:
             raise RpCMaxRetry(self.chain, "Exceeded max re-try cnt")
 
         body = {
@@ -147,9 +131,23 @@ class EthRpcClient:
         }
         headers = {'Content-type': 'application/json'}
 
+        PrometheusExporter.exporting_rpc_requested(self.chain)
+        self.call_num += 1
+
+        response = requests.post(self.url, json=body, headers=headers)
+        code = response.status_code
+        if code < 200 or 400 < code:
+            raise RpcOutOfStatusCode(self.chain, "code({}), msg({})".format(code, response.content))
+
+        return response
+
+    def send_request(
+            self, method: str, params: list, cnt: int = 0, resend_on_fail: bool = False
+    ) -> Optional[Union[dict, str]]:
         while True:
             try:
-                response = self.send_request_base(body, headers)
+                cnt += 1
+                response = self.send_request_base(method, params, cnt)
                 response_json = response.json()
                 break
             except RpcOutOfStatusCode or JSONDecodeError as e:
@@ -163,8 +161,14 @@ class EthRpcClient:
                 time.sleep(self.__rpc_server_downtime_allow_sec)
 
                 # re-send the request
-                resend_notify_msg = "re-send rpc request: {}".format(body)
+                resend_notify_msg = "re-send rpc request: method({}), params({})".format(method, params)
                 global_logger.formatted_log("RPCException", related_chain=self.__chain, msg=resend_notify_msg)
+
+                if not resend_on_fail:
+                    raise e
+
+            except RpCMaxRetry:
+                raise RpCMaxRetry(self.chain, "Exceeded max re-try cnt")
 
             except Exception as e:
                 # raise not handled exception
@@ -178,118 +182,104 @@ class EthRpcClient:
         if "error" in list(response_json.keys()):
             raise_integrated_exception(self.chain, error_json=response_json["error"])
         else:
-            raise Exception("Not handled error on {}: {}".format(self.chain.name, response.content))
+            raise Exception("Not handled error on {}: {}".format(self.chain, response.content))
 
-    def _reduce_heights_to_matured_height(self, heights: Union[list, int, str]) -> Union[List[str], str]:
+    def amend_height_to_matured_height(self, height: Union[int, str]) -> Union[List[str], str]:
         """
         reduce heights whenever each height is bigger than matured height.
         note that matured height means the maximum confirmed block height.
         """
-        latest_block_height = self.eth_get_latest_block_number()
-        matured_max_height = latest_block_height - self.__block_aging_period
-
-        if not isinstance(heights, list):
-            # for single height
-            return reduce_height_to_matured_height(matured_max_height, heights)
+        matured_max_height = self.eth_get_latest_block_number(matured_only=True)
+        if height == "latest":
+            return hex(matured_max_height)
+        elif isinstance(height, int):
+            return hex(max(height, matured_max_height))
+        elif isinstance(height, str):
+            return hex(max(int(height, 16), matured_max_height))
         else:
-            # for multi heights
-            amended_heights = list()
-            for height in heights:
-                amended_height = reduce_height_to_matured_height(matured_max_height, height)
-                amended_heights.append(amended_height)
-            return amended_heights
+            raise Exception("Invalid type of height: {}".format(height))
 
     def eth_get_latest_block_number(self, matured_only: bool = False) -> int:
         """ returns the latest block height. """
-        resp = self.send_request("eth_blockNumber", list())
+        resp = self.send_request("eth_blockNumber", [])
         latest_height = int(resp, 16)
         return latest_height if not matured_only else latest_height - self.__block_aging_period
 
-    def eth_get_latest_block(self, verbose: bool = False) -> EthBlock:
-        resp = self.send_request("eth_getBlockByNumber", ["latest", verbose])
-        return EthBlock.from_dict(resp)
-
-    def eth_get_balance(self, address: EthAddress, height: Union[int, str] = "latest",
-                        matured: bool = False) -> EthAmount:
-        """ queries matured balance of the user. """
-        if not isinstance(address, EthAddress):
-            raise Exception("address type must be \"EthAddress\" type")
-        if matured:
-            height = self._reduce_heights_to_matured_height(height)
-        resp = self.send_request("eth_getBalance", [address.hex(), height])
-        return EthAmount(resp)
-
-    def _get_block(self, method: str, params: list) -> Optional[EthBlock]:
+    def _get_block(self, method: str, params: list, matured_only: bool = False) -> Optional[EthBlock]:
         resp = self.send_request(method, params)
-        return EthBlock.from_dict(resp)
+        if resp is None:
+            return resp
 
-    def _get_matured_block(self, method: str, params: list) -> Optional[EthBlock]:
-        resp = self.send_request(method, params)
-        fetched_block: EthBlock = EthBlock.from_dict(resp)
-        if fetched_block.number > self.eth_get_latest_block_number(matured_only=True):
+        block = EthBlock.from_dict(resp)
+        if matured_only and block.number >= self.eth_get_latest_block_number(matured_only=True):
             return None
-        return fetched_block
 
-    def eth_get_block_by_hash(self, block_hash: EthHashBytes, verbose: bool = False) -> Optional[EthBlock]:
+        return block
+
+    def eth_get_latest_block(self, verbose: bool = False, matured_only: bool = False) -> EthBlock:
+        latest_height = self.eth_get_latest_block_number(matured_only)
+        return self._get_block("eth_getBlockByNumber", [hex(latest_height), verbose])
+
+    def eth_get_block_by_hash(
+            self, block_hash: EthHashBytes, verbose: bool = False, matured_only: bool = False
+    ) -> Optional[EthBlock]:
         if not isinstance(block_hash, EthHashBytes):
             raise EthTypeError(EthHashBytes, type(block_hash))
-        return self._get_block("eth_getBlockByHash", [block_hash.hex(), verbose])
+        return self._get_block("eth_getBlockByHash", [block_hash.hex(), verbose], matured_only)
 
-    def eth_get_block_by_height(self, height: Union[int, str] = "latest", verbose: bool = False) -> Optional[EthBlock]:
+    def eth_get_block_by_height(
+            self, height: Union[int, str] = "latest", verbose: bool = False, matured_only: bool = False
+    ) -> Optional[EthBlock]:
         height_hex_or_latest = hex_height_or_latest(height)
-        return self._get_block("eth_getBlockByNumber", [height_hex_or_latest, verbose])
+        return self._get_block("eth_getBlockByNumber", [height_hex_or_latest, verbose], matured_only)
 
-    def _get_transaction(self, method: str, params: list) -> Optional[EthTransaction]:
+    def _get_transaction(self, method: str, params: list, matured_only: bool = False) -> Optional[EthTransaction]:
         resp = self.send_request(method, params)
+        if resp is None:
+            return None
+
         fetched_tx: EthTransaction = EthTransaction.from_dict(resp)
-        if fetched_tx.block_number > self.eth_get_latest_block_number(matured_only=True):
+        if matured_only and fetched_tx.block_number >= self.eth_get_latest_block_number(matured_only=True):
             return None
         return fetched_tx
 
-    def eth_get_transaction_by_hash(self, tx_hash: EthHashBytes) -> Optional[EthTransaction]:
+    def eth_get_transaction_by_hash(self, tx_hash: EthHashBytes, matured_only: bool = False) -> Optional[EthTransaction]:
         if not isinstance(tx_hash, EthHashBytes):
             raise EthTypeError(EthHashBytes, type(tx_hash))
-        return self._get_transaction("eth_getTransactionByHash", [tx_hash.hex()])
+        return self._get_transaction("eth_getTransactionByHash", [tx_hash.hex()], matured_only)
 
-    def eth_get_transaction_by_height_and_index(self, height: int, tx_index: int) -> Optional[EthTransaction]:
+    def eth_get_transaction_by_height_and_index(self, height: int, tx_index: int, matured_only: bool = False) -> Optional[EthTransaction]:
         if not isinstance(height, int):
             raise EthTypeError(int, type(height))
         if not isinstance(tx_index, int):
             raise EthTypeError(int, type(tx_index))
-        return self._get_transaction("eth_getTransactionByBlockNumberAndIndex", [hex(height), hex(tx_index)])
+        return self._get_transaction("eth_getTransactionByBlockNumberAndIndex", [hex(height), hex(tx_index)], matured_only)
 
-    def eth_get_transaction_by_hash_and_index(self,
-                                              block_hash: EthHashBytes,
-                                              tx_index: int) -> Optional[EthTransaction]:
+    def eth_get_transaction_by_hash_and_index(
+            self, block_hash: EthHashBytes, tx_index: int, matured_only: bool = False
+    ) -> Optional[EthTransaction]:
         if not isinstance(block_hash, EthHashBytes):
             raise EthTypeError(EthHashBytes, type(block_hash))
         if not isinstance(tx_index, int):
             raise EthTypeError(int, type(tx_index))
-        return self._get_transaction("eth_getTransactionByBlockHashAndIndex", [block_hash.hex(), hex(tx_index)])
+        return self._get_transaction("eth_getTransactionByBlockHashAndIndex", [block_hash.hex(), hex(tx_index)], matured_only)
 
-    def _get_matured_receipt(self, tx_hash: EthHashBytes) -> Optional[EthReceipt]:
+    def _get_receipt(self, tx_hash: EthHashBytes, matured_only: bool = False) -> Optional[EthReceipt]:
         resp = self.send_request('eth_getTransactionReceipt', [tx_hash.hex()])
-
         if resp is None:
             return None
+
         fetched_receipt: EthReceipt = EthReceipt.from_dict(resp)
-        if fetched_receipt.block_number > self.eth_get_latest_block_number(matured_only=True):
+        if matured_only and fetched_receipt.block_number >= self.eth_get_latest_block_number(matured_only=True):
             return None
         return fetched_receipt
 
-    def _get_receipt(self, tx_hash: EthHashBytes) -> Optional[EthReceipt]:
-        resp = self.send_request('eth_getTransactionReceipt', [tx_hash.hex()])
-        if resp is not None:
-            return EthReceipt.from_dict(resp)
+    def eth_receipt_without_wait(self, tx_hash: EthHashBytes, matured_only: bool = False) -> Optional[EthReceipt]:
+        return self._get_receipt(tx_hash, matured_only)
 
-    def eth_receipt_without_wait(self, tx_hash: EthHashBytes, matured: bool = True) -> Optional[EthReceipt]:
-        get_receipt_func: Callable = self._get_matured_receipt if matured else self._get_receipt
-        return get_receipt_func(tx_hash)
-
-    def eth_receipt_with_wait(self, tx_hash: EthHashBytes, matured: bool = True) -> Optional[EthReceipt]:
-        get_receipt_func: Callable = self._get_matured_receipt if matured else self._get_receipt
+    def eth_receipt_with_wait(self, tx_hash: EthHashBytes, matured_only: bool = False) -> Optional[EthReceipt]:
         for i in range(self.__receipt_max_try):
-            receipt = get_receipt_func(tx_hash)
+            receipt = self._get_receipt(tx_hash, matured_only)
             if receipt is not None:
                 return receipt
             time.sleep(self.__block_period_sec / 2)  # wait half block
@@ -303,7 +293,7 @@ class EthRpcClient:
         if from_block > to_block:
             raise Exception("from_block should be less than to_block")
 
-        amended_block_nums = self._reduce_heights_to_matured_height([from_block, to_block])
+        amended_block_nums = [self.amend_height_to_matured_height(height) for height in [from_block, to_block]]
 
         topic_hexes = list()
         for topic in topics:
@@ -361,6 +351,18 @@ class EthRpcClient:
     def eth_estimate_gas(self, tx: dict):
         resp = self.send_request("eth_estimateGas", [tx, "latest"])
         return int(resp, 16)
+
+    def eth_get_balance(
+            self, address: EthAddress, height: Union[int, str] = "latest", matured_only: bool = False
+    ) -> EthAmount:
+        """ queries matured balance of the user. """
+        if not isinstance(address, EthAddress):
+            raise Exception("address type must be \"EthAddress\" type")
+        if matured_only:
+            height = self.amend_height_to_matured_height(height)
+
+        resp = self.send_request("eth_getBalance", [address.hex(), height])
+        return EthAmount(resp)
 
     def eth_get_user_nonce(self, address: EthAddress, height: Union[int, str] = "latest") -> int:
         height_hex_or_latest = hex_height_or_latest(height)
--- tests/test_rpchandler.py
+++ tests/test_rpchandler.py
@@ -2,12 +2,17 @@ import os
 import unittest
 from time import sleep
 
+from chainpy.eth.ethtype.amount import EthAmount
+from chainpy.eth.ethtype.hexbytes import EthHashBytes, EthAddress
+from chainpy.eth.ethtype.transaction import EthTransaction
+from chainpy.eth.managers.consts import *
+from chainpy.eth.managers.exceptions import RpcOutOfStatusCode, RpCMaxRetry
 from tests.rpcendpointmock.procutil import kill_by_file_name
 
 from bridgeconst.consts import Chain
 from chainpy.eth.managers.rpchandler import EthRpcClient
 
-from rpcendpointmock.rpcserver import METHOD_503_ERROR, MOCK_CHAIN_ID, ENDPOINT_PORT, ENDPOINT_URL
+from rpcendpointmock.rpcserver import *
 
 
 class TestContractHandler(unittest.TestCase):
@@ -18,9 +23,12 @@ class TestContractHandler(unittest.TestCase):
 
         self.cli = EthRpcClient.from_config_files(
             "./configs-event-test/entity.test.json",
-            chain=Chain.ETH_GOERLI
+            chain=Chain.RESERVED_01.name
         )
         self.cli.url = ENDPOINT_URL
+        self.block_hash = EthHashBytes(TEST_BLOCK_HASH)
+        self.tx_hash = EthHashBytes(TEST_TRANSACTION_HASH)
+        self.tx_hash_waiting = EthHashBytes(TEST_ADDITIONAL_TX_HASH)
 
     def launch_mock_server(self):
         if kill_by_file_name(self.server_launch_file_name):
@@ -33,15 +41,125 @@ class TestContractHandler(unittest.TestCase):
         result = kill_by_file_name(self.server_launch_file_name)
         print("server down") if result else print("No server")
 
-    def test_send_request_success(self):
-        print("entered")
+    def test_cli_init(self):
+        self.assertEqual(self.cli.chain_id, int(MOCK_CHAIN_ID, 16))
+        self.assertEqual(self.cli.url, ENDPOINT_URL)
+        self.assertEqual(self.cli.chain, Chain.RESERVED_01.name)
+        self.assertEqual(self.cli.resend_delay_sec, 3)
+
+        expected_tx_commit_time = DEFAULT_BLOCK_PERIOD_SECS * (DEFAULT_RPC_TX_BLOCK_DELAY + DEFAULT_BLOCK_AGING_BLOCKS)
+        self.assertEqual(self.cli.tx_commit_time_sec, expected_tx_commit_time)
+
+    def test_send_request_base(self):
         result = self.cli.send_request("eth_chainId", [])
-        print(result)
-        self.assertTrue(isinstance(result, str))
-        self.assertEqual(result, "0xbfc0")
+        self.assertEqual(result, MOCK_CHAIN_ID)
 
-        # self.assertRaises(Exception, self.cli.send_request, "eth_chainI", [])
+        self.assertRaises(Exception, self.cli.send_request, "eth_chainI", [])
+        self.assertRaises(RpcOutOfStatusCode, self.cli.send_request_base, "server_error_503", [])
+        self.assertRaises(RpcOutOfStatusCode, self.cli.send_request_base, "server_error_429", [])
+        self.assertRaises(RpcOutOfStatusCode, self.cli.send_request_base, "server_error_404", [])
 
-    def test_send_request_503_exception(self):
-        result = self.cli.send_request("server_error_503", [])
-        print(result)
+    def test_send_request(self):
+        self.assertRaises(RpcOutOfStatusCode, self.cli.send_request, "server_error_503", [], resend_on_fail=False)
+
+        before_call = self.cli.call_num
+        self.assertRaises(RpCMaxRetry, self.cli.send_request, "server_error_503", [], resend_on_fail=True)
+        self.assertEqual(self.cli.call_num - before_call, RPC_MAX_RESEND_ITER)
+
+    def test_get_latest_block_number(self):
+        latest_height = self.cli.eth_get_latest_block_number()
+        self.assertEqual(latest_height, int(MOCK_LATEST_HEIGHT, 16))
+
+        matured_latest_height = self.cli.eth_get_latest_block_number(matured_only=True)
+        self.assertEqual(matured_latest_height, int(MOCK_LATEST_HEIGHT, 16) - self.cli.block_aging_period)
+
+    def test_get_block(self):
+        latest_height = int(MOCK_LATEST_HEIGHT, 16)
+
+        # get_block with no option
+        block = self.cli.eth_get_latest_block()
+        self.assertEqual(block.number, latest_height)
+
+        # get_block w/ verbose option
+        block = self.cli.eth_get_latest_block(verbose=True)
+        tx = block.transactions[0]
+        self.assertEqual(tx.block_number, latest_height)
+        self.assertTrue(isinstance(tx, EthTransaction))
+
+        # get_block with matured_only option - returns latest and matured block
+        block = self.cli.eth_get_latest_block(matured_only=True)
+        self.assertEqual(block.number, latest_height - self.cli.block_aging_period)
+
+        # get_block_by_height w/o matured_only option
+        block = self.cli.eth_get_block_by_height(latest_height)
+        self.assertEqual(block.number, latest_height)
+
+        # get_block_by_height w/ matured_only option
+        # block is None, that's why, the latest height is not matured.
+        block = self.cli.eth_get_block_by_height(latest_height, matured_only=True)
+        self.assertIsNone(block)
+
+        # get_block_by_hash w/o matured_only option
+        block = self.cli.eth_get_block_by_hash(self.block_hash)
+        self.assertEqual(block.number, latest_height)
+
+        # get_block_by_hash w/ matured_only option
+        # block is None, that's why, the block exists but is not matured.
+        block = self.cli.eth_get_block_by_hash(self.block_hash, matured_only=True)
+        self.assertIsNone(block)
+
+        # get_block_by_height w/o matured_only option
+        block = self.cli.eth_get_block_by_height(TEST_HEIGHT)
+        self.assertEqual(block.number, latest_height)
+
+        # get_block_by_height w/o matured_only option
+        # block is None, that's why, the block exists but is not matured.
+        block = self.cli.eth_get_block_by_height(TEST_HEIGHT, matured_only=True)
+        self.assertIsNone(block)
+
+    def test_get_transaction(self):
+        latest_height = int(MOCK_LATEST_HEIGHT, 16)
+
+        # get_transaction_by_hash with no option
+        tx = self.cli.eth_get_transaction_by_hash(self.tx_hash)
+        self.assertEqual(tx.block_number, latest_height)
+
+        # get_transaction_by_hash w/ matured_only option
+        # transaction is None, that's why, the transaction exists but is not matured.
+        tx = self.cli.eth_get_transaction_by_hash(self.tx_hash, matured_only=True)
+        self.assertIsNone(tx)
+
+        # get_transaction_by_height_and_index with no option
+        tx = self.cli.eth_get_transaction_by_height_and_index(TEST_HEIGHT, TEST_TRANSACTION_INDEX)
+        self.assertEqual(tx.block_number, latest_height)
+
+        # get_transaction_by_height_and_index w/ matured_only option
+        tx = self.cli.eth_get_transaction_by_height_and_index(TEST_HEIGHT, TEST_TRANSACTION_INDEX, matured_only=True)
+        self.assertIsNone(tx)
+
+    def test_get_receipt(self):
+        latest_height = int(MOCK_LATEST_HEIGHT, 16)
+
+        # get_receipt_without_wait with no option
+        receipt = self.cli.eth_receipt_without_wait(self.tx_hash)
+        self.assertEqual(receipt.block_number, latest_height)
+        self.assertEqual(receipt.transaction_hash, self.tx_hash)
+
+        # get_receipt_without_wait w/ matured_only option
+        receipt = self.cli.eth_receipt_without_wait(self.tx_hash, matured_only=True)
+        self.assertIsNone(receipt)
+
+        # get_receipt_with_wait, it uses GET_RECEIPT_WAIT_ITER requests
+        before_call = self.cli.call_num
+        receipt = self.cli.eth_receipt_with_wait(self.tx_hash_waiting)
+        self.assertTrue(isinstance(receipt.transaction_hash, EthHashBytes))
+        self.assertEqual(self.cli.call_num - before_call, GET_RECEIPT_WAIT_ITER)
+
+    def test_get_balance(self):
+        balance = self.cli.eth_get_balance(EthAddress("0x71480ef89538d182241039e01808f639bf0416be"), hex(1000))
+        self.assertEqual(balance.int(), 1000000000000000000)
+        self.assertTrue(isinstance(balance, EthAmount))
+
+        balance = self.cli.eth_get_balance(EthAddress("0x71480ef89538d182241039e01808f639bf0416be"), 1000)
+        self.assertEqual(balance.int(), 1000000000000000000)
+        self.assertTrue(isinstance(balance, EthAmount))
