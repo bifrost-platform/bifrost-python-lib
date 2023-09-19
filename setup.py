cc1dad863c62945e6bd7dc754520e2e37467c7a8
alstjd0921 <kwonarseus@gmail.com>
2023-09-19 21:38:33 +0900

    CCCP-227, chore: bump old dependencies version

--- setup.py
+++ setup.py
@@ -7,19 +7,19 @@ setup(
     version=__version__,
     package=find_packages,
     install_requires=[
-        "eth-abi==2.2.0",
-        "eth-keys==0.3.4",
-        "eth-account==0.5.9",
-        "requests==2.28.1",
-        "dataclasses-json==0.5.7",
-        "prometheus-client==0.15.0",
+        "eth-abi==4.2.1",
+        "eth-keys==0.4.0",
+        "eth-account==0.9.0",
+        "requests==2.31.0",
+        "dataclasses-json==0.6.0",
+        "prometheus-client==0.17.1",
         "bridgeconst @ git+https://github.com/bifrost-platform/solidity-contract-configs@0.2.12",
-        "python-dotenv==0.16.0",
-        "jsonpath-ng",
+        "python-dotenv==1.0.0",
+        "jsonpath-ng==1.6.0",
         "jsonrpcserver",
         "fastapi",
         "uvicorn",
-        "starlette",
-        "psutil"
+        "psutil",
+        "web3==6.9.0"
     ]
 )
